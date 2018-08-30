2f33582080a87d003c9b980039cf8a20009f4b01
Anton Kolhun <anton.kolhun@zoomdata.com>
2018-08-30 10:33:14 +0300

    customized spring-boot bootstrap

--- src/main/java/com/sandbox/springsleuth/AppEntryPoint.java
+++ src/main/java/com/sandbox/springsleuth/AppEntryPoint.java
@@ -1,13 +1,32 @@
 package com.sandbox.springsleuth;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class AppEntryPoint {
 
     public static void main(String[] args) {
-        SpringApplication.run(AppEntryPoint.class, args);
+        new SpringApplicationBuilder(AppEntryPoint.class)
+                .initializers(
+                        (c) -> c.getBeanFactory()
+                                .addBeanPostProcessor(
+                                        new CustomPostProcessor()))
+                //.child()
+                .build()
+                .run(args);
+        //SpringApplication.run(AppEntryPoint.class, args);
+    }
+
+    static class CustomPostProcessor implements BeanPostProcessor {
+
+        @Override
+        public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+            //custom logic here
+            return bean;
+        }
     }
 
 }
